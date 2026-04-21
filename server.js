f3e6c26cc1d4a2bc812c94cb2fb6dcf72ba1c0ad
NateTheUnavailable <nate@stevenogardner.com>
2026-04-21 11:01:10 -0600

    more fixes and stuff added

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hello');
+})
+
+const bruh = 3000;
+
+app.listen(process.env.port || bruh)
+console.log('Web Server is listening at port ' + (process.env.port || bruh));
