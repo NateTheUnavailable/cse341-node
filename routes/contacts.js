0a52bbcc9e23b50367036ac8e388ea51409a23df
NateTheUnavailable <nate@stevenogardner.com>
2026-05-01 09:49:00 -0600

    fixing extra stuff that was not needed

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const contactsController = require('../controllers/contacts.js');
+const contactsController = require('../controllers/contacts');
 
 router.get('/', contactsController.getAll);
 
